492307ae3a1d9fc5be2a57954904a37a50b419ad
n1ckfg <nick@fox-gieg.com>
2018-06-07 18:56:00 -0400

    using GeographicLib include

--- Source/GeographicLibWrapper/Private/GeographicLibWrapper.cpp
+++ Source/GeographicLibWrapper/Private/GeographicLibWrapper.cpp
@@ -7,6 +7,36 @@
 void FGeographicLibWrapperModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+    Geocentric earth(Constants::WGS84_a(), Constants::WGS84_f());
+    // Alternatively: const Geocentric& earth = Geocentric::WGS84();
+    const double homeLat = 43.32, homeLon = 73.21, homeAlt = 0.0;
+    const double offsetX = 0.03, offsetY = -0.01, offsetZ = 0.0;
+    double scale = 1000.0;
+    
+    LocalCartesian proj(homeLat, homeLon, homeAlt, earth);
+ 
+    // XYZ to LLA
+    double lat, lon, alt;
+    proj.Reverse(offsetX * scale, offsetY * scale, offsetZ * scale, lat, lon, alt);
+    
+
+    // LLA to XYZ
+    double x, y, z;
+    proj.Forward(lat, lon, alt, x, y, z);
+    x /= scale;
+    y /= scale;
+    z /= scale;
+
+	/*
+    cout << "~ ~ ~ cpp ~ ~ ~" << "\n";
+    cout << "LLA home point: " << homeLat << " " << homeLon << " " << homeAlt << "\n";
+    cout << "+ XYZ offset: " << offsetX << " " << offsetY << " " << offsetZ << "\n";
+    cout << " . . . . . . . ." << "\n";
+    cout << "As LLA: " << lat << " " << lon << " " << alt << "\n";
+    cout << "Back to XYZ: " << x << " " << y << " " << z << "\n";
+    cout << "~ ~ ~ ~ ~ ~ ~ ~" << "\n";
+	*/
 }
 
 void FGeographicLibWrapperModule::ShutdownModule()
--- Source/GeographicLibWrapper/Public/GeographicLibWrapper.h
+++ Source/GeographicLibWrapper/Public/GeographicLibWrapper.h
@@ -5,6 +5,16 @@
 #include "CoreMinimal.h"
 #include "ModuleManager.h"
 
+#include <stdio.h>
+#include <iostream>
+#include <exception>
+#include <cmath>
+#include <GeographicLib/Geocentric.hpp>
+#include <GeographicLib/LocalCartesian.hpp>
+
+using namespace std;
+using namespace GeographicLib;
+
 class FGeographicLibWrapperModule : public IModuleInterface
 {
 public:
